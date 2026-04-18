0981a26fdcca101a7770c41d947b908bd1bdb7ec
William Kennedy <bill@ardanlabs.com>
2026-04-17 20:06:13 -0400

    fixed 404 error with templates (#480)

--- sdk/tools/catalog/templates_download.go
+++ sdk/tools/catalog/templates_download.go
@@ -100,6 +100,10 @@ func (t *templates) templateListGitHubFolder(ctx context.Context) ([]string, err
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, nil
+	}
+
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("list-git-hub-folder: unexpected status: %s", resp.Status)
 	}
